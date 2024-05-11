64e3311752b700e52aa4d15178e435a25826661b
bhanufyi <bhanuprasadcherukuvada2000@gmail.com>
2024-05-12 00:23:49 +0530

    Revert "feat(about): update education details"
    This reverts commit 872b3856524f0f125d0578baa65e9ad02f2e5836.

--- src/lib/components/Experience.tsx
+++ src/lib/components/Experience.tsx
@@ -196,26 +196,12 @@ const experiences = [
         }}
       >
         <li>
-          <strong>Core Member of Google DSC (2020-2021):</strong> Conducted
-          workshops on web and app development, machine learning, and
-          competitive coding for 2nd and 3rd year students. Gave sessions on
-          Git, GitHub actions, and open source contribution.
+          Developed an online compiler feature for programming assignments using
+          Sphere Engine API with parallel execution of test cases.
         </li>
         <li>
-          <strong>IE Student Coordinator (2018-2022):</strong> Organized several
-          events including Bellman Crown, a national level coding competition, a
-          web design contest, reverse coding event, and career guidance sessions
-          with alumni.
-        </li>
-        <li>
-          <strong>Cisco Campus Ambassador (2021-2022):</strong> Assisted final
-          year students in securing internships through a partnership between
-          CISCO and AICTE. Facilitated courses on cybersecurity, packet tracer,
-          and cybersecurity essentials.
-        </li>
-        <li>
-          <strong>Street Cause Member, Social Impact Team (2020-2021):</strong>{" "}
-          Helped raise funds to support the needy during the COVID-19 pandemic.
+          Fixed the breaking changes in the Material UI Components in the front
+          end.
         </li>
       </ListWrapper>
     ),
